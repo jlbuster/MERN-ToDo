2e5a5dd9e5309d14a185f14df6206e84a013b8df
John Blume <jlblumework@gmail.com>
2020-11-16 14:23:11 -0500

    Small commit to make sure when a new post is created, it is posted right away

--- client/src/App.js
+++ client/src/App.js
@@ -47,6 +47,7 @@ class App extends React.Component {
       .then(() => {
         console.log('Data has been sent to the server')
         this.resetUserInputs()
+        this.getBlogPost()
       })
       .catch(() => {
         console.log('Internal server error')
